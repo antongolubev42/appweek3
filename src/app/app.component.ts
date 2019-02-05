68a152088ce2f8d01bcf2862a6c2de77c40a97a7
Anton Golubev <anton.g421@gmail.com>
2019-02-05 17:25:52 +0000

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'root-directive',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-first-apps';
+  name= 'anton';
+  age= '100';
+  //componenta='This is component A ';
+  //componentb='This is component B';
 }
